ab1a57f3c9fdd59df9f6187ea20b487e517cb994
Arvind20012004 <142274938+Arvind20012004@users.noreply.github.com>
2023-09-19 19:18:31 +0000

    adding

---
+++ basics01/01variables.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail= "engrarvindak@gmail.com"
+var accountPassword="12345"
+accountCity="banda"
+
+// change the topic 
+accountEmail="kakashyap@gmail.com"
+accountPassword="121212"
+accountCity="badausa"
+
+// prefer note to use var becouse of issue in block scope and function scope 
+
+console.table({accountId, accountEmail, accountPassword, accountCity})
